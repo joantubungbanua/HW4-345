1322f9efa6127821fd69cac6bc3d81506958cc05
Joan Marie Tubungbanua <jtubungb@stevens.edu>
2021-03-30 15:14:35 -0400

    modify index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var urlRoot = "https://api.github.com";
 // NCSU Enterprise endpoint:
 //var urlRoot = "https://api.github.ncsu.edu";
 
-var userId = "yy2111";
+var userId = "joantubungbanua";
 var config = {};
 // Retrieve our api token from the environment variables.
 config.token = process.env.GITHUBTOKEN;
@@ -25,7 +25,7 @@ if (process.env.NODE_ENV != 'test')
 {
 	(async () => {
 		await listAuthenicatedUserRepos();
-		//await listBranches(userId, "your repo");
+		await listBranches(userId, "SSW345_NodeLab");
 		//await createRepo(userId,newrepo);
 		//await createIssue(userId, repo, issue);
 		//await enableWikiSupport(userId,repo);
@@ -94,12 +94,26 @@ function listAuthenicatedUserRepos()
 // 1. Write code for listBranches in a given repo under an owner. See list branches
 async function listBranches(owner,repo)
 {
-	let options = getDefaultOptions(`/`, "GET");
+	let options = getDefaultOptions(`/repos/${owner}/${repo}/branches`, "GET");
 
 	// Send a http request to url and specify a callback that will be called upon its return.
 	return new Promise(function(resolve, reject)
 	{
 		request(options, function (error, response, body) {
+
+			if( error )
+			{
+				console.log( chalk.red(error));
+				reject(error);
+				return; //Terminate execution
+			}
+
+			var obj = JSON.parse(body);
+			for( var i = 0 ;i < obj.length; i++)
+			{
+				var name = obj[i].name;
+				console.log( name );
+			}
 
 			// console.debug( options );
 			resolve( JSON.parse(body) );
